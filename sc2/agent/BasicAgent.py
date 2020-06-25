c54a219cf6dd9deea2efb04f13e8502a79641b9e
parksurk <parksurk@gmail.com>
2020-06-25 22:41:25 +0900

    update STEP1 notebook and sc2.agent.BasicAgent.py

--- sc2/agent/BasicAgent.py
+++ sc2/agent/BasicAgent.py
@@ -22,14 +22,25 @@ class TerranBasicAgent(base_agent.BaseAgent):
 
         return [x + x_distance, y + y_distance]
 
+    def getMeanLocation(self, unitList):
+        sum_x = 0
+        sum_y = 0
+        for unit in unitList:
+            sum_x += unit.x
+            sum_y += unit.y
+        mean_x = sum_x / len(unitList)
+        mean_y = sum_y / len(unitList)
+
+        return [mean_x, mean_y]
+
     def unit_type_is_selected(self, obs, unit_type):
         if (len(obs.observation.single_select) > 0 and
-            obs.observation.single_select[0].unit_type == unit_type):
-              return True
+                obs.observation.single_select[0].unit_type == unit_type):
+            return True
 
         if (len(obs.observation.multi_select) > 0 and
-            obs.observation.multi_select[0].unit_type == unit_type):
-              return True
+                obs.observation.multi_select[0].unit_type == unit_type):
+            return True
 
         return False
 
@@ -43,42 +54,49 @@ class TerranBasicAgent(base_agent.BaseAgent):
     def step(self, obs):
         super(TerranBasicAgent, self).step(obs)
 
-        time.sleep(0.5)
+        # time.sleep(0.5)
 
-        if self.base_top_left is None:
-            player_y, player_x = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.SELF).nonzero()
-            self.base_top_left = player_y.mean() <= 31
+        if obs.first():
+            self.base_top_left = None
+            self.supply_depot_built = False
+            self.barracks_built = False
+            self.barracks_rallied = False
+            self.army_rallied = False
+
+            player_y, player_x = (
+                    obs.observation.feature_minimap.player_relative == features.PlayerRelative.SELF).nonzero()
+            self.base_top_left = 1 if player_y.any() and player_y.mean() <= 31 else 0
 
         if not self.supply_depot_built:
             if self.unit_type_is_selected(obs, units.Terran.SCV):
                 if self.can_do(obs, actions.FUNCTIONS.Build_SupplyDepot_screen.id):
                     ccs = self.get_units_by_type(obs, units.Terran.CommandCenter)
                     if len(ccs) > 0:
-                        cc = random.choice(ccs)
-                        target = self.transformLocation(cc.x, 0, cc.y, 20)
+                        mean_x, mean_y = self.getMeanLocation(ccs)
+                        target = self.transformLocation(int(mean_x), 0, int(mean_y), 20)
                         self.supply_depot_built = True
 
                         return actions.FUNCTIONS.Build_SupplyDepot_screen("now", target)
             scvs = self.get_units_by_type(obs, units.Terran.SCV)
             if len(scvs) > 0:
                 scv = random.choice(scvs)
-                return actions.FUNCTIONS.select_point("select_all_type", (scv.x,
-                                                                          scv.y))
+                return actions.FUNCTIONS.select_point("select", (scv.x,
+                                                                 scv.y))
         elif not self.barracks_built:
             if self.unit_type_is_selected(obs, units.Terran.SCV):
                 if self.can_do(obs, actions.FUNCTIONS.Build_Barracks_screen.id):
                     ccs = self.get_units_by_type(obs, units.Terran.CommandCenter)
                     if len(ccs) > 0:
-                        cc = random.choice(ccs)
-                        target = self.transformLocation(cc.x, 20, cc.y, 0)
+                        mean_x, mean_y = self.getMeanLocation(ccs)
+                        target = self.transformLocation(int(mean_x), 20, int(mean_y), 0)
                         self.barracks_built = True
 
                         return actions.FUNCTIONS.Build_Barracks_screen("now", target)
             scvs = self.get_units_by_type(obs, units.Terran.SCV)
             if len(scvs) > 0:
                 scv = random.choice(scvs)
-                return actions.FUNCTIONS.select_point("select_all_type", (scv.x,
-                                                                          scv.y))
+                return actions.FUNCTIONS.select_point("select", (scv.x,
+                                                                 scv.y))
 
         elif not self.barracks_rallied:
             if self.unit_type_is_selected(obs, units.Terran.Barracks):
@@ -91,8 +109,8 @@ class TerranBasicAgent(base_agent.BaseAgent):
             barracks = self.get_units_by_type(obs, units.Terran.Barracks)
             if len(barracks) > 0:
                 barrack = random.choice(barracks)
-                return actions.FUNCTIONS.select_point("select_all_type", (barrack.x,
-                                                                          barrack.y))
+                return actions.FUNCTIONS.select_point("select", (barrack.x,
+                                                                 barrack.y))
         elif obs.observation.player.food_cap - obs.observation.player.food_used:
             if self.can_do(obs, actions.FUNCTIONS.Train_Marine_quick.id):
                 return actions.FUNCTIONS.Train_Marine_quick("queued")
